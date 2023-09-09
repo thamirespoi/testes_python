27d344449758c7f7bd092a8e6b22bc588e1dde5b
Thamires <thamy.03@hotmail.com>
2023-09-09 10:52:13 -0300

    Criei arquivo de Hello

---
+++ hello.py
@@ -0,0 +1,8 @@
+print('Hello World!')
+
+nome = "Thamires"
+profissao = "arquiteta"
+print (f"Este é o primeiro programa da {nome}")
+print ("Eu sou", profissao, "e decidi trilhar os caminhos do python")
+
+reposta = input("Você está pronto para vir comigo? (S/N)")
