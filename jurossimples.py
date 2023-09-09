bb69dd4eb31df955a757911ef5a00bc8063d9854
Thamires <thamy.03@hotmail.com>
2023-09-09 14:25:53 -0300

    Mudanças de escrita

--- jurossimples.py
+++ jurossimples.py
@@ -5,9 +5,9 @@ print("======= CALCULADORA DE JUROS SIMPLES ========")
 print("=============================================")
 
 nome = input("Olá, qual é seu nome?")
-valorprincipal = float(input("Qual é o valor principal?"))
-taxadejurosanual = float(input("Qual a taxa de juros anual?"))
-tempoanual = int(input("Qual o período de tempo em anos?"))
+valorprincipal = float(input("Digite o valor inicial:"))
+taxadejurosanual = float(input("Digite a taxa anual:"))
+tempoanual = float(input("Digite o tempo em anos:"))
 
 taxadejurosanualporcentagem = taxadejurosanual / 100
 montante = valorprincipal + (valorprincipal * taxadejurosanualporcentagem * tempoanual)
@@ -15,7 +15,7 @@ valordojuros = montante - valorprincipal
 
 print("=============================================")
 print(f"Olá {nome},")
-print(f"Seu valor principal é {valorprincipal}, e sua taxa de juros anual é {taxadejurosanual}% e você irá pagar em {tempoanual} anos")
+print(f"Seu valor principal é {valorprincipal}, sua taxa de juros anual é {taxadejurosanual}% e você irá pagar em {tempoanual} anos")
 print(f"O valor total pago é {montante}.")
 print(f"E o valor apenas do juros pago é {valordojuros}.")
 
