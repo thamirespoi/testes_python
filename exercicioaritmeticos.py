b9b6a2a762f8eec2e28a3531cd2750e8ad424cd6
Thamires <thamy.03@hotmail.com>
2023-09-09 15:05:10 -0300

    Exercício Aritméticos

---
+++ exercicioaritmeticos.py
@@ -0,0 +1,36 @@
+## Exercício Aritméticos
+
+A = 7
+B = 2
+C = 4
+D = 14
+
+Conta1 = A + B - C
+print(float(Conta1))
+
+Conta2 = A * B / C
+print(float(Conta2))
+
+Conta3 = C + 22 * D
+print(float(Conta3))
+
+Conta4 = A + B * C + D
+print(float(Conta4))
+
+Conta5 = (A + B) * (C + D)
+print(float(Conta5))
+
+Conta6 = (D / A) / (B / C)
+print(float(Conta6))
+
+Conta7 = 10 ** A
+print(float(Conta7))
+
+Conta8 = 58 % A
+print(float(Conta8))
+
+Conta9 = (A + B + C * D) / (B - C * (B - C))
+print(float(Conta9))
+
+Conta10 = (A + B + C * D) / (B - C * (B - C)) + 10 ** A
+print(float(Conta10))
