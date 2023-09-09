ab5e8e5d9ee8bb77ff4ee934dc73e4538c69d6c7
Thamires <thamy.03@hotmail.com>
2023-09-09 12:14:51 -0300

    Alterações de escrita da calculadora

--- imc.py
+++ imc.py
@@ -1,5 +1,9 @@
 # Arquivo de IMC
 
+
+print("CALCULADORA DO IMC")
+print("===============================")
+
 nome = input("Qual é seu nome?")
 idade = int(input("Qual é sua idade?"))
 peso = float(input("Qual é seu peso?"))
@@ -8,6 +12,8 @@ altura = float(input("Qual é sua altura?"))
 resultado1 = altura * altura
 imc = peso / resultado1
 
-print("Olá", nome)
-print("Sua idade é de", idade, "anos.")
-print("Seu IMC é de", imc)
+print("===============================")
+
+print(f"Olá {nome};")
+print(f"Sua idade é de {idade} anos.")
+print(f"Seu IMC é de {imc};")
