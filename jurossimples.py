725ce193c1e7adcb3a19372127d1ade94968d74c
Thamires <thamy.03@hotmail.com>
2023-09-09 12:47:42 -0300

    Aprimoramento do calculo

--- jurossimples.py
+++ jurossimples.py
@@ -4,11 +4,16 @@ print("=============================================")
 print("======= CALCULADORA DE JUROS SIMPLES ========")
 print("=============================================")
 
+nome = input("Olá, qual é seu nome?")
 valorprincipal = float(input("Qual é o valor principal?"))
 taxadejurosanual = float(input("Qual a taxa de juros anual?"))
 tempoanual = int(input("Qual o período de tempo em anos?"))
 
 montante = valorprincipal + (valorprincipal + taxadejurosanual + tempoanual)
+valordojuros = montante - valorprincipal
 
 print("=============================================")
-print(f"O valor do montante final é {montante}.")
+print(f"Olá {nome},")
+print(f"Seu valor principal é {valorprincipal}, e sua taxa de juros anual é {taxadejurosanual}% e você irá pagar em {tempoanual} anos")
+print(f"O valor do seu montante final é {montante}.")
+print(f"E valor apenas do juros pago é {valordojuros}.")
