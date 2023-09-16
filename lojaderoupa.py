dc6256bacf73511de7b2176cb3a1cd34a6c4a2ae
Thamires <thamy.03@hotmail.com>
2023-09-16 15:23:11 -0300

    Alteração na loja de roupa

--- lojaderoupa.py
+++ lojaderoupa.py
@@ -13,24 +13,26 @@ formadepagamento = int(input("Qual é a forma de pagamento? (Digite 1 se for à 
 ## FORMA DE PAGAMENTO - A VISTA
 
 if (formadepagamento == 1):
-    if (valorcompra >= 1 and valorcompra <= 499):
-        valorfinal = valorcompra - valorcompra * 0.10
-    elif (valorcompra >= 500):
-        valorfinal = valorcompra - valorcompra * 0.15
+    if (valorcompra >= 1 and valorcompra < 500):
+        desconto = 10
+    elif (valorcompra >= 500 and valorcompra < 1000):
+        desconto = 15
     elif (valorcompra >= 1000):
-        valorfinal = valorcompra - valorcompra * 0.20
+        desconto = 20
     else:
-        print("Digite uma operação válida")
+        print("Digite um valor válido")
 
 ## FORMA DE PAGAMENTO - A PRAZO 
 # ATÉ R$799,00
 
-if (formadepagamento == 2):
+elif (formadepagamento == 2):
     if (valorcompra >= 1 and valorcompra < 800): 
         print("A compra pode ser dividida até em 5x sem juros")
         parcelavezes = int(input("Digite o número de vezes que você quer dividir:")) 
         if(parcelavezes > 5 and parcelavezes >= 0):
-            print("Valor de parcelamento indisponível.")   
+            print("Valor de parcelamento indisponível.")  
+    else:
+        print("Digite um valor válido")         
 
 ## FORMA DE PAGAMENTO - A PRAZO 
 # ACIMA DE R$800,00
@@ -38,24 +40,22 @@ if (formadepagamento == 2):
     if (valorcompra >= 800):
         print("Com esse valor há mais formas de parcelamento, em até 10x sem juros e até em 18x com juros")
         parcelavezes = int(input("Digite o número de vezes que você quer dividir:"))
-        if (parcelavezes >= 1 and parcelavezes <= 10):
-            valorfinal = valorcompra
-        elif (parcelavezes == 11):
-            juros = valorcompra * (parcelavezes*0.05)
+        if (parcelavezes == 11):
+            digitojuros = 5
         elif (parcelavezes == 12):
-            juros = valorcompra * (parcelavezes*0.065)           
+            digitojuros = 6.5          
         elif (parcelavezes == 13):
-            juros = valorcompra * (parcelavezes*0.07)
+            digitojuros = 7
         elif (parcelavezes == 14):
-            juros = valorcompra * (parcelavezes*0.09)
+            digitojuros = 9
         elif (parcelavezes == 15):
-            juros = valorcompra * (parcelavezes*0.095)
+            digitojuros = 9.5
         elif (parcelavezes == 16):
-            juros = valorcompra * (parcelavezes*0.010)
+            digitojuros = 10
         elif (parcelavezes == 17):
-            juros = valorcompra * (parcelavezes*0.0113)
+            digitojuros = 11.3
         elif (parcelavezes == 18):
-            juros = valorcompra * (parcelavezes*0.012)      
+            digitojuros = 12     
         else:
             print("Essa quantidade de vezes não esta disponível para parcelamento, escolha uma quantidade disponível.")
 else:
@@ -64,15 +64,15 @@ else:
 ## PÓS VENDA
 
 if (formadepagamento == 1):
-    print(f"O valor total da compra fica {valorcompra} e o valor com desconto {valorfinal}.") 
+    print(f"O valor total da compra sem desconto é R${valorcompra} e com desconto de {desconto}% do pagamento a vista fica um total de R${valorcompra - valorcompra * (desconto / 100)}.") 
 elif (formadepagamento == 2):
-        if(valorcompra >= 1 and valorcompra <= 799):
+        if(valorcompra >= 1 and valorcompra < 800):
             if(parcelavezes >=1 and parcelavezes <= 5):
-                print(f"O valor total da compra fica {valorcompra} e o valor da parcela fica {valorcompra / parcelavezes} em {parcelavezes} parcelas.") 
+                print(f"O valor total da compra é R${valorcompra} e o valor da parcela fica R${valorcompra / parcelavezes} em {parcelavezes} parcelas.") 
         elif(valorcompra >= 800):
             if(parcelavezes >=1 and parcelavezes <= 10):
-                print(f"O valor total da compra fica {valorcompra} e o valor da parcela fica {valorcompra + juros / parcelavezes} em {parcelavezes} parcelas e o valor final com juros fica {valorcompra + juros}.")   
+               print(f"O valor total da compra é R${valorcompra} e o valor da parcela fica R${valorcompra / parcelavezes} em {parcelavezes} parcelas.")   
             if(parcelavezes >=11 and parcelavezes <= 18):
-                print(f"O valor total da compra fica {valorcompra} e o valor da parcela fica {valorcompra + juros / parcelavezes} em {parcelavezes} parcelas e o valor final com juros fica {valorcompra + juros}.") 
+                print(f"O valor total da compra é R${valorcompra}, o valor da parcela fica R${valorcompra + (valorcompra * (parcelavezes*(digitojuros / 100))) / parcelavezes} em {parcelavezes} parcelas e o valor final com juros de {digitojuros}% ao mês fica R${valorcompra + (valorcompra * (parcelavezes*(digitojuros / 100)))}.") 
 
-print(f"{nome}, obrigada pela compra, volte sempre!")
+print(f"Obrigada pela compra {nome}, volte sempre!")
